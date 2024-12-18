00708e1687456f8eea075671a9e8d20afb39c4c8
yarow <yarowilliams402@gmail.com>
2024-12-18 14:34:51 +0000

    Initial commit

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,39 @@
-public class Book {
+package org.example;
 
-    // This is your working class.
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author){
+        this.author=author;
+    }
+
+    public int getYearPublished(){
+        return year;
+    }
+    public void setYearPublished(int year){
+        this.year=year;
+    }
     
 }
+
+    
+
